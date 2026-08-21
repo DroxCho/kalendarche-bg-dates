8099297b6b8af8c4883db0ffb11a6a8be62498db
DroxCho <58247878+DroxCho@users.noreply.github.com>
2026-08-21 17:30:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    outdir: ".dist",
-  },
-  //{ mode }) => ({
- // server: {
-   // host: "::",
-   // port: 8080,
+export default defineConfig(({ mode }) => ({
+ server: {
+   host: "::",
+   port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
